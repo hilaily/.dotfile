2ca70f2df8319882e6ced297a0a33cab8f5e0144
Laily <i@laily.net>
2019-01-25 15:39:46 +0800

    fix: evn

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!bin/sh
-git clone https://github.com/Laily123/.dotfile.git ~/.dotfile
-export ZSH=~/.oh-my-zsh
-sudo yum update && sudo yum -y install zsh
+#git clone https://github.com/Laily123/.dotfile.git ~/.dotfile
+export ZSH=$HOME/.oh-my-zsh
+#sudo yum update && sudo yum -y install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 ln -sf ~/.dotfile/.zshrc ~/
 ln -sf ~/.dotfile/laily.zsh-theme ~/.oh-my-zsh/themes/
