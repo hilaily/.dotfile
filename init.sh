67007d68433a78369525ed6a72d2c7888195a524
Laily <i@laily.net>
2019-11-13 12:05:35 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!bin/sh
-#git clone https://github.com/Laily123/.dotfile.git ~/.dotfile
-export ZSH=$HOME/.oh-my-zsh
+#git clone https://github.com/hilaily/.dotfile.git ~/.dotfile
+
+# 判断 zsh 是否安装
 #sudo yum update && sudo yum -y install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# 判断 oh-my-zsh 是否安装
+if [ ! -d "$HOME/.oh-my-zsh" ];then
+    export ZSH=$HOME/.oh-my-zsh
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
 ln -sf ~/.dotfile/.zshrc ~/
 ln -sf ~/.dotfile/laily.zsh-theme ~/.oh-my-zsh/themes/
 ln -sf ~/.dotfile/.tmux.conf ~/
 ln -sf ~/.dotfile/.gitconfig ~/.gitconfig
-
