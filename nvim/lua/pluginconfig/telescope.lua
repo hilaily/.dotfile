19e2ef081c0abbba2597d44cf87aa179e4362d80
Laily <i@laily.net>
2022-01-16 18:14:15 +0800

    fix: fzf extention

--- nvim/lua/pluginconfig/telescope.lua
+++ nvim/lua/pluginconfig/telescope.lua
@@ -76,6 +76,6 @@ require('telescope').setup {
     }
 }
 
-require('telescope').load_extension('fzf')
+--require('telescope').load_extension('fzf')
 require'telescope'.load_extension('project')
 require("telescope").load_extension "file_browser"
