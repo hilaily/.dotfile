fa7a287da502ea8f9dfffa675c76b3073e3269c2
Laily <i@laily.net>
2022-09-11 22:59:47 +0800

    feat: add lua-dev

--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -1,1 +1,3 @@
+ vim.cmd('set ts=2') -- Insert 2 spaces for a tab
+ vim.cmd('set sw=2') -- Change the number of space characters inserted for indentation
 require('lang/lua')
---
+++ nvim/lua/pluginconfig/lua-dev.lua
@@ -0,0 +1,3 @@
+require("lua-dev").setup({
+	-- add any options here, or leave empty to use the default settings
+})
