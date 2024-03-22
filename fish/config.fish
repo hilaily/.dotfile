e430d2a72188fc5c4c5840f2768a74293caa6a95
Laily <i@laily.net>
2024-03-22 11:06:57 +0800

    fix: add env

--- fish/config.fish
+++ fish/config.fish
@@ -7,14 +7,14 @@ set -gx HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
 
 # go
 set -gx GOPATH $HOME/go:$HOME/workrepo/go
-set -gx GOPROXY goproxy.cn
+set -gx GOPROXY "goproxy.cn,direct"
 set -gx GOSUMDB "sum.golang.google.cn"
 
-set -gx PATH $PATH $HOME/go/bin 
+set -gx PATH $PATH $HOME/go/bin
 set -gx PATH $PATH $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin
-set -gx PATH $PATH "/usr/local/sbin"
+set -gx PATH $PATH /usr/local/sbin
 # add nvim
-set -gx PATH $PATH /usr/local/nvim/bin /usr/local/osx-64/nvim/bin 
+set -gx PATH $PATH /usr/local/nvim/bin /usr/local/osx-64/nvim/bin
 set -gx EDITOR vim
 
 # alias 
@@ -27,7 +27,7 @@ alias swg="swag init -o docs -g "
 alias sws="swagger serve -F=swagger"
 
 function gtr
-  go test -gcflags=all=-l -v -run="$1"
+    go test -gcflags=all=-l -v -run="$1"
 end
 
 
