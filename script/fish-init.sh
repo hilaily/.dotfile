8c5df554eda40f40c480bd190a385b99d8f9f03e
Laily <i@laily.net>
2024-06-11 23:06:19 +0800

    chrore: update fish script

--- script/fish-init.sh
+++ script/fish-init.sh
@@ -2,15 +2,11 @@
 
 echo 'Link fish config file'
 if [ -f "$HOME/.config/fish/config.fish" ]; then
-  mv $HOME/.config/fish/config.fish $HOME/.config/fish/config.fish.bak
+	mv $HOME/.config/fish/config.fish $HOME/.config/fish/config.fish.bak
 fi
 ln -s $HOME/.dotfile/fish/config.fish $HOME/.config/fish/config.fish
-
-echo 'Sourcing'
-source ~/.config/fish/config.fish
 
 echo 'Set fish as default shell'
 fishpath=$(which fish)
 echo $fishpath | sudo tee -a /etc/shells
 chsh -s $fishpath
-
