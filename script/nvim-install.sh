ff4629cafe1e5a160df50371970417574f4d9a1f
Laily <i@laily.net>
2024-10-25 18:02:02 +0800

    feat: modify install nvim

--- script/nvim-install.sh
+++ script/nvim-install.sh
@@ -1,4 +1,24 @@
+#!/bin/bash
+
 # install neovim
-cd /usr/local
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz
-tar zxvf nvim-macos.tar.gz 
+os_arch=$(uname -s)-$(uname -m)
+
+case $os_arch in
+  "Darwin-x86_64")
+	cd /usr/local
+	wget https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz
+	tar zxvf nvim-macos.tar.gz 
+	;;
+  "Linux-x86_64"|"Linux-arm64")
+	cd /tmp
+	rm -rf nvim-linux64.tar.gz
+	wget https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
+        tar zxf nvim-linux64.tar.gz
+	sudo mv nvim-linux64 /usr/local/nvim
+	sudo ln -s /usr/local/nvim/bin/nvim /usr/local/bin/vim
+	;;
+esac
+        
+	
+
+
