eaa9c6d924d9607095be7695354cb0e29ab6c19c
Laily <i@laily.net>
2024-10-25 18:13:08 +0800

    feat: add iterm2

---
+++ iterm2/set-config-dir.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfile/iterm2"
+# then open iterm2, Preferences->General->Settings, Mark 'load preferences from xxx'
