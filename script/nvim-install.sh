aec3961983a401517f56818e59de46b55b6e61af
Laily <i@laily.net>
2024-11-06 22:57:50 +0800

    feat: add font

--- script/nvim-install.sh
+++ script/nvim-install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+set -e
+
 # install neovim
 os_arch=$(uname -s)-$(uname -m)
 ver=v0.10.2
-
-if [ "$EUID" -ne 0 ]; then
-	echo "need sudo"
-	exit 1
-fi
 
 case $os_arch in
   "Darwin-x86_64")
@@ -15,16 +12,24 @@ case $os_arch in
 	rm -rf nvim-macos-x86_64.tar.gz
 	wget https://github.com/neovim/neovim/releases/download/$ver/nvim-macos-x86_64.tar.gz
 	tar zxf nvim-macos-x86_64.tar.gz 
+	sudo mv -f /usr/local/nvim /usr/local/nvim-bak
 	sudo mv nvim-macos-x86_64 /usr/local/nvim
 	sudo ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/vim
+	sudo ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/nvim
+	echo ""
+	vim -v
 	;;
   "Linux-x86_64"|"Linux-arm64")
 	cd /tmp
 	rm -rf nvim-linux64.tar.gz
 	wget https://github.com/neovim/neovim/releases/download/$ver/nvim-linux64.tar.gz
 	tar zxf nvim-linux64.tar.gz
+	sudo mv -f /usr/local/nvim /usr/local/nvim-bak
 	sudo mv nvim-linux64 /usr/local/nvim
 	sudo ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/vim
+	sudo ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/nvim
+	echo ""
+	vim -v
 	;;
 esac
         
---
+++ script/python-install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+# 设置颜色
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# 检查是否已安装 pyenv
+if ! command -v pyenv &> /dev/null
+then
+    echo -e "${RED}pyenv 未安装. 正在安装 pyenv...${NC}"
+    # 安装 pyenv
+    curl https://pyenv.run | bash
+else
+    echo -e "${GREEN}pyenv 已安装${NC}"
+fi
+
+# 询问用户想安装的 Python 版本
+python_version=3.11.10
+
+# 安装指定版本的 Python
+echo -e "${GREEN}正在安装 Python $python_version...${NC}"
+pyenv install $python_version
+
+# 设置全局 Python 版本
+pyenv global $python_version
+
+# 验证安装
+installed_version=$(python --version)
+echo -e "${GREEN}Python 安装完成. 当前版本: $installed_version${NC}"
+
+# 安装一些常用的 Python 包
+echo -e "${GREEN}正在安装一些常用的 Python 包...${NC}"
+pip install --upgrade pip
+pip install ipython jupyter notebook pandas numpy matplotlib scikit-learn
+
+echo -e "${GREEN}Python 环境设置完成!${NC}"
