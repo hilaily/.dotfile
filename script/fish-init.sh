a55e3ca1e2eef693485e2185eecbe00b84d0c0e8
laily <i@laily.net>
2025-02-19 17:21:00 +0800

    feat: add gvm

--- script/fish-init.sh
+++ script/fish-init.sh
@@ -2,7 +2,7 @@
 
 echo 'Link fish config file'
 if [ -f "$HOME/.config/fish/config.fish" ]; then
-	mv $HOME/.config/fish/config.fish $HOME/.config/fish/config.fish.bak
+    mv $HOME/.config/fish/config.fish $HOME/.config/fish/config.fish.bak
 fi
 ln -s $HOME/.dotfile/fish/config.fish $HOME/.config/fish/config.fish
 
--- script/go-install.sh
+++ script/go-install.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 set -e
 
-# install neovim
-os_arch=$(uname -s)-$(uname -m)
+# install gvm
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 
-case $os_arch in
-  "Darwin-x86_64")
-	cd /usr/local
-	wget https://golang.google.cn/dl/go1.23.2.linux-amd64.tar.gz
-	tar zxvf go1.23.2.linux-amd64.tar.gz 
-	;;
-  "Linux-x86_64"|"Linux-arm64")
-	cd /tmp
-	rm -rf nvim-linux64.tar.gz
-	wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
-	tar zxvf go1.23.2.linux-amd64.tar.gz 
-	mv go /usr/local/
-	sudo ln -s /usr/local/go/bin/go/usr/local/bin/go
-	;;
-esac
-        
-	
+# install gvm on fish
+fish -c "fisher install JGAntunes/fish-gvm"
+fish -c "fisher install edc/bass"
 
+# install go
+fish -c "gvm install go1.24.0"
+fish -c "gvm use go1.24.0 --default"
 
+# # install neovim
+# os_arch=$(uname -s)-$(uname -m)
+
+# case $os_arch in
+#   "Darwin-x86_64")
+# 	cd /usr/local
+# 	wget https://golang.google.cn/dl/go1.23.2.linux-amd64.tar.gz
+# 	tar zxvf go1.23.2.linux-amd64.tar.gz
+# 	;;
+#   "Linux-x86_64"|"Linux-arm64")
+# 	cd /tmp
+# 	rm -rf nvim-linux64.tar.gz
+# 	wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
+# 	tar zxvf go1.23.2.linux-amd64.tar.gz
+# 	mv go /usr/local/
+# 	sudo ln -s /usr/local/go/bin/go/usr/local/bin/go
+# 	;;
+# esac
