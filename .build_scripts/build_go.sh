8de75b7faf42b4d0d4a2e248726c712a652ffa5f
laily <i@laily.net>
2025-11-11 20:10:47 +0800

    feat: update golangci-lint config

--- .build_scripts/build_go.sh
+++ .build_scripts/build_go.sh
@@ -6,7 +6,30 @@
 
 set -e
 
-OUTPUT_DIR=${1:-./output}
+if [ -z $GOOS ]; then
+	echo "GOOS is not set"
+	exit 1
+fi
+if [ -z $GOARCH ]; then
+	echo "GOARCH is not set"
+	exit 1
+fi
 
-mkdir -p $OUTPUT_DIR/$GOOS-$GOARCH
-go build -o $OUTPUT_DIR/$GOOS-$GOARCH/server ./cmd/
+OUTPUT_DIR=${1:-./output/$GOOS-$GOARCH}
+
+mkdir -p $OUTPUT_DIR/
+go build -o $OUTPUT_DIR/server ./cmd/
+
+if [ -d ./conf ]; then
+	mkdir -p $OUTPUT_DIR/conf/
+	cp -r ./conf/* $OUTPUT_DIR/conf/
+fi
+
+if [ -d ./scripts ]; then
+	mkdir -p $OUTPUT_DIR/scripts/
+	cp -r ./scripts/* $OUTPUT_DIR/scripts/
+fi
+
+if [ -f ./scripts/Makefile ]; then
+	cp ./scripts/Makefile $OUTPUT_DIR/
+fi
