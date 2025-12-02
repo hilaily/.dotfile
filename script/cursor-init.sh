7f087b9e5f2be5c34d392bf967398a3a326217c4
laily <i@laily.net>
2025-12-02 16:23:42 +0800

    fix: cursor config

--- script/cursor-init.sh
+++ script/cursor-init.sh
@@ -7,11 +7,11 @@ src="$HOME/Library/Application Support/Cursor/User/settings.json"
 if [ -f "$src" ]; then
 	mv "$src" "$src.bak"
 fi
-ln -s "$HOME/.dotfile/vscode/settings.json" "$src"
+ln -s "$HOME/.dotfile/cursor/config/settings.json" "$src"
 
 # init keybindings.json
 src="$HOME/Library/Application Support/Cursor/User/keybindings.json"
 if [ -f "$src" ]; then
 	mv "$src" "$src.bak"
 fi
-ln -s "$HOME/.dotfile/vscode/keybindings.json" "$src"
+ln -s "$HOME/.dotfile/cursor/config/keybindings.json" "$src"
