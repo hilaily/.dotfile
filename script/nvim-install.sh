6abd1b71858974da074e9b0f4730aa0022a307b3
laily <i@laily.net>
2026-05-14 17:22:24 +0800

    fix: neovim install support aarch64

--- script/nvim-install.sh
+++ script/nvim-install.sh
@@ -31,10 +31,10 @@ case $os_arch in
     echo ""
     vim -v
     ;;
-"Linux-x86_64" | "Linux-arm64" | "Linux-aarch64")
+"Linux-x86_64")
     cd /tmp
     rm -rf nvim-linux64.tar.gz
-    wget https://github.com/neovim/neovim/releases/download/$ver/nvim-linux64.tar.gz
+    wget https://github.com/neovim/neovim/releases/download/$ver/nvim-linux-x86_64.tar.gz
     tar zxf nvim-linux64.tar.gz
     [ -e /usr/local/nvim ] && sudo mv -f /usr/local/nvim /usr/local/nvim-bak
     sudo mv nvim-linux64 /usr/local/nvim
@@ -43,6 +43,19 @@ case $os_arch in
     echo ""
     vim -v
     ;;
+"Linux-arm64" | "Linux-aarch64")
+    cd /tmp
+    rm -rf nvim-linux64.tar.gz
+    wget https://github.com/neovim/neovim/releases/download/$ver/nvim-linux-arm64.tar.gz
+    tar zxf nvim-linux64.tar.gz
+    [ -e /usr/local/nvim ] && sudo mv -f /usr/local/nvim /usr/local/nvim-bak
+    sudo mv nvim-linux64 /usr/local/nvim
+    sudo ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/vim
+    sudo ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/nvim
+    echo ""
+    vim -v
+    ;;
+
 *)
     echo "不支持的平台: $os_arch" >&2
     exit 1
